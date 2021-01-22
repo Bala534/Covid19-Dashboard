d37f83a070deed0fb15ca02846f9ba283bbf8b16
Bala Mahesh <72618397+Bala534@users.noreply.github.com>
2021-01-22 20:08:29 +0530

    Add files via upload

---
+++ build/precache-manifest.c241520978a4babf84d81d65908de269.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f3a27d35e1fd06a6db083a2063c41c",
+    "url": "/Covid-19-Global-Dashboard/index.html"
+  },
+  {
+    "revision": "a3f7776309a345156785",
+    "url": "/Covid-19-Global-Dashboard/static/css/2.861f4f67.chunk.css"
+  },
+  {
+    "revision": "70395fce37cb6f76eebe",
+    "url": "/Covid-19-Global-Dashboard/static/css/main.b73a0ad5.chunk.css"
+  },
+  {
+    "revision": "a3f7776309a345156785",
+    "url": "/Covid-19-Global-Dashboard/static/js/2.f1d0b238.chunk.js"
+  },
+  {
+    "revision": "f5d1eb698ffbfd217dac8de6c2e7a797",
+    "url": "/Covid-19-Global-Dashboard/static/js/2.f1d0b238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70395fce37cb6f76eebe",
+    "url": "/Covid-19-Global-Dashboard/static/js/main.1d765b3e.chunk.js"
+  },
+  {
+    "revision": "04a4c741f958b6c4c538",
+    "url": "/Covid-19-Global-Dashboard/static/js/runtime-main.b9979c03.js"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/Covid-19-Global-Dashboard/static/media/github1.0ba2aa20.png"
+  },
+  {
+    "revision": "b9bb7cd7405d487aee26fe56c948e074",
+    "url": "/Covid-19-Global-Dashboard/static/media/india.b9bb7cd7.png"
+  },
+  {
+    "revision": "2d7d3b9986ff9fa55bc24d932923d748",
+    "url": "/Covid-19-Global-Dashboard/static/media/virus.2d7d3b99.gif"
+  },
+  {
+    "revision": "37c09ef33571d7ec39692fb2ae7eb0c0",
+    "url": "/Covid-19-Global-Dashboard/static/media/world.37c09ef3.png"
+  }
+]);
