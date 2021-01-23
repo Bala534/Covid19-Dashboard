9e53365116ac324ee34027ccacff9d1fadb5a711
Bala Mahesh <72618397+Bala534@users.noreply.github.com>
2021-01-23 11:28:11 +0530

    Add files via upload

---
+++ src/components/ui/Header/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Github from "../../../assets/github1.png";
+import IndiaIcon from "../../../assets/india.png";
+import World from "../../../assets/world.png";
+import "./Header.css";
+
+const Header = (props) => {
+  return (
+    <div>
+      <div className="Header">
+        Covid-19 {props.dashboard} {props.width < 1024 ? "" : " Dashboard"}
+        <a
+          href="https://github.com/Bala534/Covid19-Dashboard"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Github} alt="github" className="github-icon" />
+        </a>
+      </div>
+      <div className="Switch-dashboard">
+        <img
+          onClick={() => props.history.push("/india")}
+          src={IndiaIcon}
+          alt="India"
+          className="india-icon"
+        />
+        <span style={{ fontWeight: "bold" }}>Switch</span>
+        <img
+          onClick={() => props.history.push("/")}
+          src={World}
+          alt="World"
+          className="global-icon"
+        />
+        
+
+      </div>
+    </div>
+  );
+};
+
+export default Header;
