1a3c4d1319c983eb1dcf5172fbdca59a7c768bb8
Bala Mahesh <72618397+Bala534@users.noreply.github.com>
2021-01-24 18:57:06 +0530

    Delete src/components directory

--- src/components/ui/GradientCardFooter/GradientCardFooter.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./GradientCardFooter.css";
-
-const GradientCardFooter = (props) => {
-  return <div className="GradientCardFooter">{props.content}</div>;
-};
-
-export default GradientCardFooter;
